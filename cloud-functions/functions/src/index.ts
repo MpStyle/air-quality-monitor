c5d45064a77f895ea4f53050e9a3d42950c2b281
Michele Pagnin <mpstyle.micene@gmail.com>
2019-10-27 09:05:29 +0100

    Published device-data-ingestion service

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -3,11 +3,10 @@ import admin = require('firebase-admin');
 import express = require('express');
 import { Logging } from './book/Logging';
 import { middlewareAuthorization } from './book/MiddlewareAuthorization';
-// import { deviceAdd } from './service/DeviceAdd';
 import { deviceSearch } from './service/DeviceSearch';
 import { healthCheck } from './service/HealthCheck';
-// import { measurementAdd } from './service/MeasurementAdd';
 import { measurementSearch } from './service/MeasurementSearch';
+import { deviceDataIngestion } from './service/DeviceDataIngestion';
 
 admin.initializeApp(functions.config().firebase);
 
@@ -17,10 +16,9 @@ const logging = Logging.get();
 
 app.use(cors);
 
-// app.put('/device-add', (req, res) => { middlewareAuthorization(logging)(req.body, deviceAdd(logging)).then(response => res.json(response)).catch(error => { res.send(error) }); });
 app.post('/device-search', (req, res) => { middlewareAuthorization(logging)(req.body, deviceSearch(logging)).then(response => res.json(response)).catch(error => { res.send(error) }); });
-// app.put('/measurement-add', (req, res) => { middlewareAuthorization(logging)(req.body, measurementAdd(logging)).then(response => res.json(response)).catch(error => { res.send(error) }); });
 app.post('/measurement-search', (req, res) => { middlewareAuthorization(logging)(req.body, measurementSearch(logging)).then(response => res.json(response)).catch(error => { res.send(error) }); });
 app.get('/health-check', (req, res) => { res.send(healthCheck(logging)()); });
+app.get('/device-data-ingestion', (req, res) => { res.send(deviceDataIngestion(logging)(req.body)); });
 
 exports.app = functions.https.onRequest(app);
