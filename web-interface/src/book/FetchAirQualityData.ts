f5b291d534f1b623e241bb4dd76a3f8643ddb81b
Michele Pagnin <mpstyle.micene@gmail.com>
2019-11-02 22:11:25 +0100

    Web Interface - Improved error handling

--- web-interface/src/book/FetchAirQualityData.ts
+++ web-interface/src/book/FetchAirQualityData.ts
@@ -6,12 +6,18 @@ export const fetchAirQualityData = (currentDeviceId: string, dispatch: Dispatch<
     const url = process.env.REACT_APP_AIR_QUALITY_DATA_REMOTE_URL as string;
     fetch(`${url}/airQualityData?_page=1&_limit=1&_sort=inserted&_order=desc&deviceId=${currentDeviceId}`)
         .then((response): Promise<AirQualityData[]> => {
+            if (!response.ok) {
+                console.error(`Error while fetch air quality data`);
+                return Promise.resolve([]);
+            }
+
             return response.json();
         })
         .then((response: AirQualityData[]) => {
             dispatch(updateAirQualityDataActionBuilder(response[0]));
         })
-        .catch((_error) => {
+        .catch((error) => {
             // TODO: dispatch an error message
+            console.error(`Error while fetch air quality data: ${error}`);
         });
 };
--- web-interface/src/book/FetchDevices.ts
+++ web-interface/src/book/FetchDevices.ts
@@ -7,12 +7,18 @@ export const fetchDevices = (dispatch: Dispatch<Action>) => {
     const url = process.env.REACT_APP_AIR_QUALITY_DATA_REMOTE_URL as string;
     fetch(`${url}/device`)
         .then((response): Promise<Device[]> => {
+            if (!response.ok) {
+                console.error(`Error while fetch devices`);
+                return Promise.resolve([]);
+            }
+
             return response.json();
         })
         .then((response: Device[]) => {
             dispatch(updateDevicesActionBuilder(response));
         })
-        .catch((_error) => {
+        .catch((error) => {
             // TODO: dispatch an error message
+            console.error(`Error while fetch devices: ${error}`);
         });
 };
