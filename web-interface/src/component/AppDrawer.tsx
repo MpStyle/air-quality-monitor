7996d8a7e7cb05da3afc6848f163b2b9152554b4
Michele Pagnin <pagnin.michele@gmail.com>
2019-12-15 00:10:30 +0100

    Web Interface - Added link to Credits page in drawer

--- web-interface/src/component/AppDrawer.tsx
+++ web-interface/src/component/AppDrawer.tsx
@@ -1,19 +1,26 @@
 import Drawer from "@material-ui/core/Drawer/Drawer";
 import List from "@material-ui/core/List/List";
 import ListItem from "@material-ui/core/ListItem/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText/ListItemText";
+import InfoIcon from '@material-ui/icons/Info';
 import React, { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import "./AppDrawer.scss";
 
 export const AppDrawer: FunctionComponent<AppDrawerProps> = (props) => {
-    return <Drawer open={props.isOpen} onClose={props.toggleDrawer(false)}>
+    return <Drawer open={props.isOpen} onClose={props.toggleDrawer(false)} className="app-drawer">
         <div
             role="presentation"
             onClick={props.toggleDrawer(false)}
             onKeyDown={props.toggleDrawer(false)}
-        >
+            className="list-container">
             <List>
-                <ListItem button>
-                    <ListItemText primary="ciao" />
+                <ListItem button className="list-item" component={Link} to="/credits">
+                    <ListItemIcon>
+                        <InfoIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Credits" />
                 </ListItem>
             </List>
         </div>
