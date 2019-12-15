dceb2eb5f10ef70738e555a5bfe6b7dbeddbe70a
Michele Pagnin <pagnin.michele@gmail.com>
2019-12-15 01:28:57 +0100

    Web Interface - Added app icon and name in drawer

--- web-interface/src/component/AppDrawer.tsx
+++ web-interface/src/component/AppDrawer.tsx
@@ -1,8 +1,10 @@
+import Divider from "@material-ui/core/Divider/Divider";
 import Drawer from "@material-ui/core/Drawer/Drawer";
 import List from "@material-ui/core/List/List";
 import ListItem from "@material-ui/core/ListItem/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText/ListItemText";
+import Typography from "@material-ui/core/Typography/Typography";
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import InfoIcon from '@material-ui/icons/Info';
 import { History } from 'history';
@@ -10,6 +12,7 @@ import React, { FunctionComponent } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { updateSecretKeyActionBuilder } from "../action/UpdateSecretKeyAction";
 import { CREDITS_URL } from '../book/Pages';
+import logo from '../images/logo.svg';
 import { appStore } from "../store/AppStore";
 import "./AppDrawer.scss";
 
@@ -22,6 +25,18 @@ export const AppDrawer: FunctionComponent<AppDrawerProps> = (props) => {
             onClick={props.toggleDrawer(false)}
             onKeyDown={props.toggleDrawer(false)}
             className="list-container">
+
+            <div className="app">
+                <Typography variant="h6">
+                    <div className="logo-container">
+                        <img src={logo} />
+                    </div>
+                    Air Quality Monitor
+                </Typography>
+            </div>
+
+            <Divider />
+
             <List>
                 <ListItem button className="list-item" component={Link} to={CREDITS_URL}>
                     <ListItemIcon>
