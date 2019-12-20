c111faca0f75179911a5778f9ebe01282879f435
Michele Pagnin <mpstyle.micene@gmail.com>
2019-12-20 09:31:54 +0100

    Cloud Functions - Updated definition of Service

--- cloud-functions/functions/src/entity/Service.ts
+++ cloud-functions/functions/src/entity/Service.ts
@@ -1,9 +1,5 @@
 import { ServiceResponse } from "./ServiceResponse";
 
 export interface Service<TReq, TPayload> {
-    (request: TReq): ServiceResponse<TPayload>
-}
-
-export interface ServiceAsync<TReq, TPayload> {
     (request: TReq): Promise<ServiceResponse<TPayload>>
 }
