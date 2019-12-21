65ebb24c150c23c6377f6b8f9f4111f6513ce963
Michele Pagnin <mpstyle.micene@gmail.com>
2019-12-21 15:09:09 +0100

    Cloud Functions - user measurements search returns all the readings types in one response

--- cloud-functions/functions/src/service/UserMeasurementsSearch.ts
+++ cloud-functions/functions/src/service/UserMeasurementsSearch.ts
@@ -3,7 +3,8 @@ import { Errors } from "../entity/Errors";
 import { Scopes } from "../entity/Scopes";
 import { buildErrorResponse, buildResponse, Service } from "../entity/Service";
 import { authorization } from "./Authorization";
-import { MeasurementSearchRequest as MeasurementsSearchRequest, MeasurementSearchResponse as MeasurementsSearchResponse, measurementsSearch } from "./MeasurementsSearch";
+import { MeasurementSearchRequest, measurementsSearch } from "./MeasurementsSearch";
+import Bluebird = require("bluebird");
 
 export const userMeasurementsSearch = (logging: ILogging): Service<UserMeasurementsSearchRequest, UserMeasurementsSearchResponse> => req => {
     try {
@@ -27,20 +28,37 @@ export const userMeasurementsSearch = (logging: ILogging): Service<UserMeasureme
                     .filter(a => a.scopes.indexOf(Scopes.device_air_quality_data_read) !== -1)
                     .map(a => a.deviceId);
 
-                return measurementsSearch(logging)(req)
-                    .then(response => {
-                        if (response.error) {
-                            return buildErrorResponse(response.error);
+                if (deviceIds.indexOf(req.deviceId) === -1) {
+                    return buildErrorResponse(Errors.DEVICE_AUTHORIZATION_ERROR);
+                }
+
+                const measurementTypes = ['pressure', 'temperature', 'co2', 'humidity', 'tvoc'];
+
+                return Bluebird
+                    .map(measurementTypes, (measurementType, index) => {
+                        return measurementsSearch(logging)(<MeasurementSearchRequest>{ deviceId: req.deviceId, limit: 1, type: measurementType });
+                    }, { concurrency: 3 })
+                    .then(results => results.reduce((acc, curr) => {
+                        if (curr.error || !curr.payload || !curr.payload.measurements.length) {
+                            return acc;
                         }
 
-                        if (!response.payload) {
-                            return buildErrorResponse(Errors.MEASUREMENT_NOT_FOUND);
+                        const value = curr.payload.measurements[0].value ? parseFloat(curr.payload.measurements[0].value) : null;
+                        switch (curr.payload.measurements[0].type) {
+                            case 'temperature': acc.temperature = value; break;
+                            case 'co2': acc.co2 = value; break;
+                            case 'humidity': acc.humidity = value; break;
+                            case 'tvoc': acc.tvoc = value; break;
+                            case 'pressure': acc.pressure = value; break;
                         }
 
-                        return buildResponse<UserMeasurementsSearchResponse>({
-                            measurements: response.payload.measurements.filter(m => deviceIds.indexOf(m.deviceId) !== -1)
-                        });
-                    });
+                        if (!acc.inserted || acc.inserted > curr.payload.measurements[0].inserted) {
+                            acc.inserted = curr.payload.measurements[0].inserted;
+                        }
+
+                        return acc;
+                    }, {} as UserMeasurementsSearchResponse))
+                    .then(response => buildResponse(response));
             })
             .catch((err: any) => {
                 logging.error("userMeasurementSearch", `Error while searching measurements: ${err}`);
@@ -53,10 +71,16 @@ export const userMeasurementsSearch = (logging: ILogging): Service<UserMeasureme
     }
 };
 
-export interface UserMeasurementsSearchRequest extends MeasurementsSearchRequest {
+export interface UserMeasurementsSearchRequest {
     secretKey: string;
+    deviceId: string;
 }
 
-// tslint:disable-next-line: no-empty-interface
-export interface UserMeasurementsSearchResponse extends MeasurementsSearchResponse {
+export interface UserMeasurementsSearchResponse {
+    humidity: number | null;
+    temperature: number | null;
+    pressure: number | null;
+    tvoc: number | null;
+    co2: number | null;
+    inserted: number;
 }
