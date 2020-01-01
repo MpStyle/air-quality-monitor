a4f04da1c5de8350eb9424fdbeff73cb523df1b7
Michele Pagnin <mpstyle.micene@gmail.com>
2020-01-01 12:01:48 +0100

    Web Interface - Added route for not founded pages

--- web-interface/src/index.tsx
+++ web-interface/src/index.tsx
@@ -1,8 +1,8 @@
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, Typography } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import { APP_SETTINGS_URL, CREDITS_URL, DASHBOARD_URL, LOGIN_URL } from './book/Pages';
 import { AppSettingsContainer } from './component/AppSettingsContainer';
@@ -17,17 +17,20 @@ ReactDOM.render(
     <Provider store={appStore}>
         <CssBaseline />
         <HashRouter>
-            <Route exact path={CREDITS_URL} component={Credits} />
-            <Route exact path={DASHBOARD_URL}>
-                <IsLogged
-                    loginPageUrl={LOGIN_URL}
-                    sourceUrl={DASHBOARD_URL}>
-                    <DashboardContainer />
-                </IsLogged>
-            </Route>
-            <Route exact path="/" component={LoginContainer} />
-            <Route path={LOGIN_URL} component={LoginContainer} />
-            <Route path={APP_SETTINGS_URL} component={AppSettingsContainer} />
+            <Switch>
+                <Route exact path={CREDITS_URL} component={Credits} />
+                <Route exact path={DASHBOARD_URL}>
+                    <IsLogged
+                        loginPageUrl={LOGIN_URL}
+                        sourceUrl={DASHBOARD_URL}>
+                        <DashboardContainer />
+                    </IsLogged>
+                </Route>
+                <Route exact path="/" component={LoginContainer} />
+                <Route path={LOGIN_URL} component={LoginContainer} />
+                <Route path={APP_SETTINGS_URL} component={AppSettingsContainer} />
+                <Route render={() => (<Typography variant="h6" color="error">404 Not Found</Typography>)} />
+            </Switch>
         </HashRouter>
     </Provider>,
     document.getElementById('root')
