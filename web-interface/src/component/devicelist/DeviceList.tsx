d0b6e8e1536a636f2157c288a306a89e17f5a9ca
Michele Pagnin <pagnin.michele@gmail.com>
2020-02-08 17:53:08 +0100

    Web Interface - Improved devices list page

--- web-interface/src/component/devicelist/DeviceList.tsx
+++ web-interface/src/component/devicelist/DeviceList.tsx
@@ -11,6 +11,8 @@ import Paper from "@material-ui/core/Paper/Paper";
 import Typography from "@material-ui/core/Typography/Typography";
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import InfoIcon from '@material-ui/icons/Info';
 import React, { FunctionComponent, useState } from "react";
 import { Link } from "react-router-dom";
@@ -22,7 +24,24 @@ import "./DeviceList.scss";
 
 export const DeviceList: FunctionComponent<DevicesListProps> = (props) => {
     const [deviceToDelete, setDeviceToDelete] = useState<Device | null>(null);
-    const [open, setOpen] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+    const [openDetails, setOpenDetails] = useState<string[]>([]);
+
+    const toggle = (deviceId: string) => {
+        const index = openDetails.indexOf(deviceId);
+        const copy = [...openDetails];
+        if (index === -1) {
+            copy.push(deviceId);
+        }
+        else {
+            copy.splice(index, 1);
+        }
+        setOpenDetails(copy);
+    };
+
+    const isOpen = (deviceId: string): boolean => {
+        return openDetails.indexOf(deviceId) !== -1;
+    };
 
     return <div className="devices-list">
         <AppBarOneRow>
@@ -41,18 +60,40 @@ export const DeviceList: FunctionComponent<DevicesListProps> = (props) => {
             {!props.isLoading && <Paper elevation={2} className="devices-list-container">
                 {!props.devices.length && <div className="message">No devices</div>}
 
-                {props.devices && <List>
-                    {props.devices.map(d => <ListItem key={d.deviceId}>
-                        <ListItemIcon onClick={() => {
-                            setDeviceToDelete(d);
-                            setOpen(true);
-                        }}>
-                            <IconButton>
-                                <DeleteForeverIcon />
-                            </IconButton>
-                        </ListItemIcon>
-                        <ListItemText primary={d.name} />
-                    </ListItem>)}
+                {props.devices && <List className="devices-list">
+                    {props.devices.map(d => (
+                        <ListItem key={d.deviceId} className="device">
+                            <ListItemIcon
+                                onClick={() => {
+                                    toggle(d.deviceId);
+                                }}>
+                                <IconButton>
+                                    {isOpen(d.deviceId) === false ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+                                </IconButton>
+                            </ListItemIcon>
+                            <ListItemText
+                                primary={d.name}
+                                secondary={
+                                    isOpen(d.deviceId) && <React.Fragment>
+                                        <div className="ip">
+                                            <strong>IP:</strong> {d.deviceIP.split(";")[0]}
+                                        </div>
+                                        {d.address && d.address.length && <div className="address">
+                                            <strong>Address:</strong> {d.address}
+                                        </div>}
+                                    </React.Fragment>
+                                } />
+                            <ListItemIcon
+                                onClick={() => {
+                                    setDeviceToDelete(d);
+                                    setOpenModal(true);
+                                }}>
+                                <IconButton>
+                                    <DeleteForeverIcon />
+                                </IconButton>
+                            </ListItemIcon>
+                        </ListItem>
+                    ))}
                 </List>}
             </Paper>}
         </main>
@@ -60,17 +101,17 @@ export const DeviceList: FunctionComponent<DevicesListProps> = (props) => {
             className="delete-request-modal"
             aria-labelledby="simple-modal-title"
             aria-describedby="simple-modal-description"
-            open={open}
-            onClose={() => { setOpen(false); }}>
-            <Fade in={open}>
+            open={openModal}
+            onClose={() => { setOpenModal(false); }}>
+            <Fade in={openModal}>
                 <Paper elevation={2} className="delete-request">
                     <InfoIcon /> Are you sure to delete {deviceToDelete?.name}?
                     <div className="buttons">
                         <Button onClick={() => {
                             props.onDeleteClick(props.token, (deviceToDelete as Device).deviceId);
-                            setOpen(false);
+                            setOpenModal(false);
                         }}>Yes</Button>
-                        <Button onClick={() => setOpen(false)}>No</Button>
+                        <Button onClick={() => setOpenModal(false)}>No</Button>
                     </div>
                 </Paper>
             </Fade>
