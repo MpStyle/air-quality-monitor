04a152f8883fde5c8e6a32e84d5b6c94e40caff8
Michele Pagnin <pagnin.michele@gmail.com>
2020-02-09 11:55:11 +0100

    Cloud Functions - Moved controllers to separate definition file

---
+++ cloud-functions/functions/src/book/Controllers.ts
@@ -0,0 +1,73 @@
+import express = require('express');
+import { Logging } from '../book/Logging';
+import { buildErrorResponse } from '../entity/Service';
+import { deviceDataIngestion } from '../service/domain/DeviceDataIngestion';
+import { healthCheck } from '../service/domain/HealthCheck';
+import { userDevicesList } from '../service/domain/UserDevicesList';
+import { userLogin } from '../service/domain/UserLogin';
+import { userLastReadings as userLastReadings } from '../service/domain/UserLastReadings';
+import { userRenewAccessToken } from '../service/domain/UserRenewAccessToken';
+import { userRevokeRefreshToken } from '../service/domain/UserRevokeRefreshToken';
+import { userTimeRangeReadings } from '../service/domain/UserTimeRangeReadings';
+import { userDeviceDelete } from '../service/domain/UserDeviceDelete';
+
+const cors = require('cors')({ origin: true });
+const app = express();
+const logging = Logging.get();
+
+app.use(cors);
+
+app.get('/health-check', (req, res) => {
+    healthCheck(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+
+// Device - To add data
+app.put('/device-data-ingestion', (req, res) => {
+    deviceDataIngestion(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+
+// User - To retrieve data
+app.post('/user-devices-list', (req, res) => {
+    userDevicesList(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+app.post('/user-device-delete', (req, res) => {
+    userDeviceDelete(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+app.post('/user-last-readings', (req, res) => {
+    userLastReadings(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+
+app.post('/user-time-range-readings', (req, res) => {
+    userTimeRangeReadings(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+
+// User - Authentication/Authorization
+app.post('/user-login', (req, res) => {
+    userLogin(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+app.post('/user-renew-access-token', (req, res) => {
+    userRenewAccessToken(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+app.post('/user-revoke-refresh-token', (req, res) => {
+    userRevokeRefreshToken(logging)(req.body)
+        .then(data => res.send(data))
+        .catch(err => buildErrorResponse(err));
+});
+
+export const controllers = app;
--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,77 +1,7 @@
 import functions = require('firebase-functions');
 import admin = require('firebase-admin');
-import express = require('express');
-import { Logging } from './book/Logging';
-import { buildErrorResponse } from './entity/Service';
-import { deviceDataIngestion } from './service/domain/DeviceDataIngestion';
-import { healthCheck } from './service/domain/HealthCheck';
-import { userDevicesList } from './service/domain/UserDevicesList';
-import { userLogin } from './service/domain/UserLogin';
-import { userLastReadings as userLastReadings } from './service/domain/UserLastReadings';
-import { userRenewAccessToken } from './service/domain/UserRenewAccessToken';
-import { userRevokeRefreshToken } from './service/domain/UserRevokeRefreshToken';
-import { userTimeRangeReadings } from './service/domain/UserTimeRangeReadings';
-import { userDeviceDelete } from './service/domain/UserDeviceDelete';
+import { controllers } from './book/Controllers';
 
 admin.initializeApp(functions.config().firebase);
 
-const cors = require('cors')({ origin: true });
-const app = express();
-const logging = Logging.get();
-
-app.use(cors);
-
-app.get('/health-check', (req, res) => {
-    healthCheck(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-
-// Device - To add data
-app.put('/device-data-ingestion', (req, res) => {
-    deviceDataIngestion(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-
-// User - To retrieve data
-app.post('/user-devices-list', (req, res) => {
-    userDevicesList(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-app.post('/user-device-delete', (req, res) => {
-    userDeviceDelete(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-app.post('/user-last-readings', (req, res) => {
-    userLastReadings(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-
-app.post('/user-time-range-readings', (req, res) => {
-    userTimeRangeReadings(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-
-// User - Authentication/Authorization
-app.post('/user-login', (req, res) => {
-    userLogin(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-app.post('/user-renew-access-token', (req, res) => {
-    userRenewAccessToken(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-app.post('/user-revoke-refresh-token', (req, res) => {
-    userRevokeRefreshToken(logging)(req.body)
-        .then(data => res.send(data))
-        .catch(err => buildErrorResponse(err));
-});
-
-exports.app = functions.https.onRequest(app);
+exports.app = functions.https.onRequest(controllers);
