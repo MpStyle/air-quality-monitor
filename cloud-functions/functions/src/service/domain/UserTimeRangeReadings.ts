e80dfac24fd92771ba1e0b5ca921eb8f0360c224
MpStyle <mpstyle.micene@gmail.com>
2020-02-13 09:19:11 +0100

    Cloud Functions - Added capability to filter by day in user time range readings service

--- cloud-functions/functions/src/service/domain/UserTimeRangeReadings.ts
+++ cloud-functions/functions/src/service/domain/UserTimeRangeReadings.ts
@@ -1,10 +1,14 @@
+import { epochToLocaleDate } from "../../book/DateTimeUtils";
 import { ILogging } from "../../book/Logging";
 import { Errors } from "../../entity/Errors";
+import { Granularity } from "../../entity/Granularity";
 import { Scopes } from "../../entity/Scopes";
-import { buildErrorResponse, Service } from "../../entity/Service";
+import { buildErrorResponse, buildResponse, Service } from "../../entity/Service";
 import { TimeRangeReading } from '../../entity/TimeRangeReading';
 import { TimeRangeReadingSearchRequest, timeRangeReadingsSearch } from "../crud/TimeRangeReadingsSearch";
+import { StringUtils } from './../../book/StringUtils';
 import { userAuthorization } from "./UserAuthorization";
+import Bluebird = require("bluebird");
 
 export const userTimeRangeReadings = (logging: ILogging): Service<UserTimeRangeReadingsRequest, UserTimeRangeReadingsResponse> => req => {
     if (
@@ -35,12 +39,51 @@ export const userTimeRangeReadings = (logging: ILogging): Service<UserTimeRangeR
                 return buildErrorResponse(Errors.DEVICE_AUTHORIZATION_ERROR);
             }
 
-            const searchRequest: TimeRangeReadingSearchRequest = {
-                deviceId: req.deviceId,
-                type: req.type
-            };
+            let timeRangeDaily = undefined;
+            let timeRangeMonthly = undefined;
+            let timeRangeYearly = undefined;
+            if (req.timestamp) {
+                const dateTime = epochToLocaleDate(req.timestamp);
+                timeRangeDaily = dateTime.getUTCFullYear() + StringUtils.padLeft(dateTime.getUTCMonth() + 1, '0', 2) + StringUtils.padLeft(dateTime.getUTCDate() + 1, '0', 2);
+                timeRangeMonthly = dateTime.getUTCFullYear() + StringUtils.padLeft(dateTime.getUTCMonth() + 1, '0', 2);
+                timeRangeYearly = dateTime.getUTCFullYear().toString();
+            }
 
-            return timeRangeReadingsSearch(logging)(searchRequest);
+            const searchRequests: TimeRangeReadingSearchRequest[] = [
+                {
+                    deviceId: req.deviceId,
+                    type: req.type,
+                    granularity: Granularity.daily,
+                    timeRange: timeRangeDaily
+                },
+                {
+                    deviceId: req.deviceId,
+                    type: req.type,
+                    granularity: Granularity.monthly,
+                    timeRange: timeRangeMonthly
+                },
+                {
+                    deviceId: req.deviceId,
+                    type: req.type,
+                    granularity: Granularity.yearly,
+                    timeRange: timeRangeYearly
+                }
+            ];
+
+            return Bluebird
+                .map(searchRequests, (searchRequest) => timeRangeReadingsSearch(logging)(searchRequest), { concurrency: 3 })
+                .then(responses => {
+                    return responses.reduce((acc, curr) => {
+                        if (curr.payload && curr.payload.timeRangeReadings) {
+                            return acc.concat(curr.payload.timeRangeReadings);
+                        }
+
+                        return acc;
+                    }, new Array<TimeRangeReading>());
+                })
+                .then(result => {
+                    return buildResponse<UserTimeRangeReadingsResponse>({ timeRangeReadings: result });
+                });
         })
         .catch((err: any) => {
             logging.error("userTimeRangeReadings", `Error while searching time range readings: ${err}`);
@@ -52,6 +95,12 @@ export interface UserTimeRangeReadingsRequest {
     accessToken: string;
     deviceId: string;
     type: string;
+
+    /**
+     * (UTC) Epoch in milliseconds of the client.
+     * Used to fileter the data.
+     */
+    timestamp: number;
 }
 
 export interface UserTimeRangeReadingsResponse {
