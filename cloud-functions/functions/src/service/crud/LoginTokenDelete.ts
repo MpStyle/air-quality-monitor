7b56ab58b7295d96d85893b5477b2205798b83f5
Michele Pagnin <pagnin.michele@gmail.com>
2020-02-15 15:37:05 +0100

    Cloud Functions - Edited login token delete service

--- cloud-functions/functions/src/service/crud/LoginTokenDelete.ts
+++ cloud-functions/functions/src/service/crud/LoginTokenDelete.ts
@@ -1,40 +1,21 @@
 import { ILogging } from "../../book/Logging";
 import { Collections } from "../../entity/Collections";
-import { Service, buildErrorResponse, buildResponse } from "../../entity/Service";
+import { buildResponse, Service, buildErrorResponse } from "../../entity/Service";
 import admin = require('firebase-admin');
-import { loginTokenSearch } from "./LoginTokenSearch";
-import { LoginToken } from "../../entity/LoginToken";
 import { Errors } from "../../entity/Errors";
-import Bluebird = require("bluebird");
 
-export const loginTokenDelete = (logging: ILogging): Service<LoginTokenDeleteRequest, LoginTokenDeleteResponse> => req => {
+export const loginTokenDelete = (logging: ILogging): Service<LoginTokenDeleteRequest, {}> => req => {
+    if (!req.refreshToken || req.refreshToken === '') {
+        return buildErrorResponse(Errors.INVALID_LOGIN_TOKEN_DELETE_REQUEST);
+    }
+
     logging.info("loginTokenDelete", "Starts");
 
     const db = admin.firestore();
 
-    return loginTokenSearch(logging)({ refreshToken: req.refreshToken })
-        .then(response => {
-            if (response.error) {
-                return buildErrorResponse(response.error);
-            }
-
-            if (!response.payload || !response.payload.loginTokens) {
-                return buildErrorResponse(Errors.ERROR_WHILE_SEARCH_LOGIN_TOKEN);
-            }
-
-            return Bluebird
-                .map(response.payload.loginTokens, (loginToken) => (
-                    db.collection(Collections.LOGIN_TOKEN).doc(loginToken.refreshToken).delete().then(_ => loginToken)
-                ), { concurrency: 5 })
-                .then(results => buildResponse<LoginTokenDeleteResponse>({ deletedLoginTokens: results }));
-        });
-
+    return db.collection(Collections.LOGIN_TOKEN).doc(req.refreshToken).delete().then(_ => buildResponse({}));
 };
 
 export interface LoginTokenDeleteRequest {
     refreshToken: string
-}
-
-export interface LoginTokenDeleteResponse {
-    deletedLoginTokens: LoginToken[];
 }
