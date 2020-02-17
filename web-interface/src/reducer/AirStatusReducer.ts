e46b0d70cf5310529331664324011b70ba418d24
Michele Pagnin <pagnin.michele@gmail.com>
2020-02-17 17:40:18 +0100

    Web Interface - Stored status data in local storage

--- web-interface/src/reducer/AirStatusReducer.ts
+++ web-interface/src/reducer/AirStatusReducer.ts
@@ -1,5 +1,7 @@
 import { Action } from "redux";
 import { UpdateAirStatusAction, UpdateAirStatusActionName } from "../action/UpdateAirStatusAction";
+import { LocalStorageKey } from "../book/LocalStorageKey";
+import { localStorageManager } from "../book/LocalStorageManager";
 import { AirStatus } from "../entity/AirStatus";
 import { initialAppState } from "../store/InitialAppState";
 
@@ -7,6 +9,12 @@ export const airStatusReducer = (state: AirStatus = initialAppState.airStatus, a
     switch (action.type) {
         case UpdateAirStatusActionName:
             const updateAirStatusAction = action as UpdateAirStatusAction;
+
+            localStorageManager.setItem(LocalStorageKey.CO2_STATUS_KEY, updateAirStatusAction.airStatus.co2);
+            localStorageManager.setItem(LocalStorageKey.HUMIDITY_STATUS_KEY, updateAirStatusAction.airStatus.humidity);
+            localStorageManager.setItem(LocalStorageKey.PRESSURE_STATUS_KEY, updateAirStatusAction.airStatus.pressure);
+            localStorageManager.setItem(LocalStorageKey.TEMPERATURE_STATUS_KEY, updateAirStatusAction.airStatus.temperature);
+            localStorageManager.setItem(LocalStorageKey.TVOC_STATUS_KEY, updateAirStatusAction.airStatus.tvoc);
 
             return updateAirStatusAction.airStatus;
     }
