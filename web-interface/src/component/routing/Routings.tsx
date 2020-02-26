e142419318685b13e4776f877a9038a1c756b717
MpStyle <mpstyle.micene@gmail.com>
2020-02-26 10:32:19 +0100

    Web Simulator - Moved routing to own file

---
+++ web-interface/src/component/routing/Routings.tsx
@@ -0,0 +1,58 @@
+import React, { FunctionComponent } from 'react';
+import { Route, Switch } from 'react-router';
+import { HashRouter } from 'react-router-dom';
+import { Pages } from '../../book/Pages';
+import { NotFoundPage } from '../404/PageNotFound';
+import { ChartsContainer } from '../charts/ChartsContainer';
+import { IsLogged } from '../common/IsLogged';
+import { AppConsoleContainer } from '../console/AppConsoleContainer';
+import { Credits } from '../credits/Credits';
+import { DashboardContainer } from '../dashboard/DashboardContainer';
+import { DeviceListContainer } from '../devicelist/DeviceListContainer';
+import { LoginContainer } from '../login/LoginContainer';
+import { AppSettingsContainer } from '../settings/AppSettingsContainer';
+
+export const Routing: FunctionComponent<{}> = (_props) => {
+    return <HashRouter>
+        <Switch>
+            <Route exact path="/" component={LoginContainer} />
+            <Route exact path={Pages.LOGIN_URL} component={LoginContainer} />
+            <Route exact path={Pages.CREDITS_URL} component={Credits} />
+            <Route exact path={Pages.APP_SETTINGS_URL} component={AppSettingsContainer} />
+
+            <Route exact path={Pages.DASHBOARD_URL}>
+                <IsLogged
+                    loginPageUrl={Pages.LOGIN_URL}
+                    sourceUrl={Pages.DASHBOARD_URL}>
+                    <DashboardContainer />
+                </IsLogged>
+            </Route>
+
+            <Route exact path={Pages.DEVICE_LIST_URL}>
+                <IsLogged
+                    loginPageUrl={Pages.LOGIN_URL}
+                    sourceUrl={Pages.DEVICE_LIST_URL}>
+                    <DeviceListContainer />
+                </IsLogged>
+            </Route>
+
+            <Route path={`${Pages.CHARTS_URL}/:readingType`}>
+                <IsLogged
+                    loginPageUrl={Pages.LOGIN_URL}
+                    sourceUrl={Pages.CHARTS_URL}>
+                    <ChartsContainer />
+                </IsLogged>
+            </Route>
+
+            <Route exact path={`${Pages.APP_CONSOLE_URL}`}>
+                <IsLogged
+                    loginPageUrl={Pages.LOGIN_URL}
+                    sourceUrl={Pages.APP_CONSOLE_URL}>
+                    <AppConsoleContainer />
+                </IsLogged>
+            </Route>
+
+            <Route component={NotFoundPage} />
+        </Switch>
+    </HashRouter>;
+};
--- web-interface/src/index.tsx
+++ web-interface/src/index.tsx
@@ -2,19 +2,8 @@ import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router';
-import { HashRouter } from 'react-router-dom';
 import { Colors } from './book/Colors';
-import { Pages } from './book/Pages';
-import { NotFoundPage } from './component/404/PageNotFound';
-import { ChartsContainer } from './component/charts/ChartsContainer';
-import { IsLogged } from './component/common/IsLogged';
-import { AppConsoleContainer } from './component/console/AppConsoleContainer';
-import { Credits } from './component/credits/Credits';
-import { DashboardContainer } from './component/dashboard/DashboardContainer';
-import { DeviceListContainer } from './component/devicelist/DeviceListContainer';
-import { LoginContainer } from './component/login/LoginContainer';
-import { AppSettingsContainer } from './component/settings/AppSettingsContainer';
+import { Routing } from './component/routing/Routings';
 import './index.scss';
 import { appStore } from './store/AppStore';
 
@@ -38,31 +27,7 @@ ReactDOM.render(
     <Provider store={appStore}>
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <HashRouter>
-                <Switch>
-                    <Route exact path={Pages.CREDITS_URL} component={Credits} />
-                    <Route exact path={Pages.DASHBOARD_URL}>
-                        <IsLogged
-                            loginPageUrl={Pages.LOGIN_URL}
-                            sourceUrl={Pages.DASHBOARD_URL}>
-                            <DashboardContainer />
-                        </IsLogged>
-                    </Route>
-                    <Route exact path={Pages.DEVICE_LIST_URL}>
-                        <IsLogged
-                            loginPageUrl={Pages.LOGIN_URL}
-                            sourceUrl={Pages.DEVICE_LIST_URL}>
-                            <DeviceListContainer />
-                        </IsLogged>
-                    </Route>
-                    <Route exact path="/" component={LoginContainer} />
-                    <Route path={Pages.LOGIN_URL} component={LoginContainer} />
-                    <Route path={Pages.APP_SETTINGS_URL} component={AppSettingsContainer} />
-                    <Route path={`${Pages.CHARTS_URL}/:readingType`} component={ChartsContainer} />
-                    <Route path={`${Pages.APP_CONSOLE_URL}`} component={AppConsoleContainer} />
-                    <Route component={NotFoundPage} />
-                </Switch>
-            </HashRouter>
+            <Routing />
         </ThemeProvider>
     </Provider>,
     document.getElementById('root')
