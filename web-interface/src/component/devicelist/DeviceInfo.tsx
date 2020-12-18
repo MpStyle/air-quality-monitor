baacc849b1eb1369e5ff50e74531b997ad89e465
Michele Pagnin <mpstyle.micene@gmail.com>
2020-12-18 22:21:03 +0100

    Web Interface - Improved device data visualization

--- web-interface/src/component/devicelist/DeviceInfo.tsx
+++ web-interface/src/component/devicelist/DeviceInfo.tsx
@@ -3,11 +3,9 @@ import ListItem from "@material-ui/core/ListItem/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText/ListItemText";
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
-import React, { useState, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
 import { Device } from "../../entity/Device";
 import { MeterUnit } from "../../entity/MeterUnit";
@@ -15,7 +13,6 @@ import "./DeviceInfo.scss";
 
 export const DeviceInfo: FunctionComponent<DeviceInfoProps> = props => {
     const { t } = useTranslation();
-    const [isDetailsOpen, setIsDetailsOpen] = useState<boolean>(false);
     const isThereAddress = props.device.address && props.device.address.length;
     const isThereIp = !!props.device.ip && !!props.device.ip.length;
     const isThereSSID = !!props.device.wifiName && !!props.device.wifiName.length;
@@ -23,28 +20,25 @@ export const DeviceInfo: FunctionComponent<DeviceInfoProps> = props => {
 
     return <ListItem className="device">
         {(isThereIp || isThereAddress) && <ListItemIcon
-            onClick={() => setIsDetailsOpen(!isDetailsOpen)}>
+            onClick={() => props.onCurrentDeviceChange(props.device)}>
             <IconButton>
-                {isDetailsOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-            </IconButton>
-        </ListItemIcon>}
-        <ListItemText
-            primary={<div className="device-name" onClick={() => props.onCurrentDeviceChange(props.device)}>
                 {props.isCurrentDevice ?
                     <RadioButtonCheckedIcon /> :
                     <RadioButtonUncheckedIcon />}
-                {props.device.name}
-            </div>}
+            </IconButton>
+        </ListItemIcon>}
+        <ListItemText
+            primary={<div className="device-name">{props.device.name}</div>}
             secondary={
-                isDetailsOpen && <React.Fragment>
+                <React.Fragment>
                     {isThereIp && <div className="ip">
                         <strong>{t("ip")}:</strong> {props.device.ip}
                     </div>}
                     {isThereSSID && <div className="ssid">
-                        <strong>{t("ssi")}:</strong> {props.device.wifiName}
+                        <strong>{t("ssid")}:</strong> {props.device.wifiName}
                     </div>}
                     {isThereSignalStrenght && <div className="wifi-strenght">
-                        <strong>{t("wifi-strenght")}:</strong> {props.device.wifiSignalStrength}
+                        <strong>{t("wifi-strenght")}:</strong>&nbsp;{100 * (1 - (-40 - props.device.wifiSignalStrength) / (-40 - 40))}%
                     </div>}
                     {isThereAddress && <div className="address">
                         <strong>{t("address")}:</strong> {props.device.address}
